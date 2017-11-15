669a05f4df6e654cc1641520f642531c8e020326
James Ferguson <ncjamesferguson@gmail.com>
2017-11-15 14:33:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var gen = [];
+  for (var i = 0; i < 4; i++) {
+    gen.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return gen;
+}
+function johnLennonFacts(facts) {
+  var i = 0;
+  var factR = []
+  while (i < facts.length - 1) {
+    factR.push(`${facts[i]}!!!`)
+    i++
+  }
+  return factR
+}
